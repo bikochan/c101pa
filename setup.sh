095bfcda6d0d2136c59eb3842152ba74462fec52
Andre Faure <andre.faure@camelotglobal.com>
2018-04-17 17:07:07 +0100

    fix local connection and path to python add --noconfirm to pacman commands

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 set -x -e -o pipefail
 
 pkgs="git ansible"
-pacman -Syu --color=auto
-pacman -S --color=auto ${pkgs}
+pacman -Syu --color=auto --noconfirm
+pacman -S --color=auto --noconfirm ${pkgs}
 
 git clone https://bitbucket.org/bikochan/c101pa.git
-cd c101pa
 
 cat <<EOT
 Edit values in the file named `all` to customise your system
 then run:
 
+   cd c101pa
    ansible-playbook -v setup.yaml
 
 EOT
